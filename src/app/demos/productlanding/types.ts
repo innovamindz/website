eb401661735816f44ce5bbfc8417248f294f5711
AbdAllah <abdallah.boda13@gmail.com>
2025-07-25 17:41:44 +0300

    chore: remove unneeded pages

--- src/app/demos/productlanding/types.ts
+++
@@ -1,9 +0,0 @@
-import { StaticImageData } from 'next/image'
-import { IconType } from 'react-icons'
-
-export type FeatureType = {
-  icon: IconType
-  image?: StaticImageData
-  title: string
-  description?: string
-}
